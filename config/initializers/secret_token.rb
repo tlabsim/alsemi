67aac371af8af8ad7c98ce30ec0b8fe526b64e2d
tlabsim <im_tlabs@yahoo.com>
2013-10-05 20:37:05 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alsemi::Application.config.secret_token = '13b391469dfac4eeafed64a5e813c93cfd63699f79c5c43141413f38284de738d5655d33669883ae9bf2697195f4dc606453d5a124092ec3c2ba54b9071dc6bf'
